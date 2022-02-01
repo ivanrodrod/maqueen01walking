be85fa91e2664af18964870023a46ef42a581f4f
ivanrodrod <80984814+ivanrodrod@users.noreply.github.com>
2022-02-01 19:21:03 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
 basic.forever(function () {
-	
+    basic.showIcon(IconNames.Heart)
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 120)
+    basic.pause(1000)
+    basic.showIcon(IconNames.SmallHeart)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+    basic.pause(300)
 })
